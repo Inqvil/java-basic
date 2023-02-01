f4303605d70d928e2b0f7e0112c20be228ad6a50
samat <samat_akhmedov@mail.ru>
2023-02-02 01:03:49 +0400

    hm4 - банк

---
+++ src/ru/otus/homework/hm6/Client.java
@@ -0,0 +1,34 @@
+package ru.otus.homework.hm6;
+
+import java.util.Objects;
+
+public class Client {
+    private String name;
+    private int age;
+
+    Client(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return age == client.age && Objects.equals(name, client.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
